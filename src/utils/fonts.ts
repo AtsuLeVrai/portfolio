88075eba7e81dca6b466ea974deadefca7591907
AtsumiFlex <minecraftytom22@gmail.com>
2024-03-15 23:38:32 +0100

    Add ESLint and Tailwind configurations, utility functions and more
    Added new ESLint and Tailwind configuration files, several utility functions, and corresponding dependencies to the project. Also created new pages for the application and initialized appropriate scripts in package.json. This implementation is crucial for setting up initial development settings, ensuring code quality, managing styles, and organizing the structure of the application.

---
+++ src/utils/fonts.ts
@@ -0,0 +1,8 @@
+import { Fira_Code, Montserrat } from "next/font/google";
+
+export const montserrat = Montserrat({
+	subsets: ["latin"],
+	weight: "500",
+});
+
+export const fira_code = Fira_Code({ subsets: ["latin"] });
