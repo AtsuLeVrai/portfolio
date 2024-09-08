bcf5919b29ddc3f5a7167ed47a5b59a50c579428
3tatsu <minecraftytom22@gmail.com>
2024-09-08 21:06:13 +0200

    chore: update dependencies and font configuration
    - Modify package.json and pnpm-lock.yaml
    - Update font configuration in src/lib/fonts.ts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,16 +1,10 @@
-import { Eczar, Lexend, Poppins } from "next/font/google";
-import localFont from "next/font/local";
+import { Eczar, Poppins } from "next/font/google";
 
-export const helvetica = localFont({
-    src: "../fonts/helvetica/helvetica.ttf",
-    variable: "--font-helvetica",
-    weight: "100 900",
+export const eczar = Eczar({
+    subsets: ["latin"],
 });
-
-export const lexend = Lexend();
-
-export const eczar = Eczar();
 
 export const poppins = Poppins({
     weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    subsets: ["latin"],
 });
