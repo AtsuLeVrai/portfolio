c9545d64f7f5354d572eb827726a21f60a368813
AtsuLeVrai <minecraftytom22@gmail.com>
2025-11-04 16:04:59 +0100

    feat: remove Experience section from main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { About } from "@/app/sections/About";
 import { Contact } from "@/app/sections/Contact";
-import { Experience } from "@/app/sections/Experience";
 import { Hero } from "@/app/sections/Hero";
 import { Projects } from "@/app/sections/Projects";
 import { Footer as LayoutFooter } from "@/components/layout/Footer";
@@ -14,7 +13,6 @@ export default function HomePage() {
 				<Hero />
 				<About />
 				<Projects />
-				<Experience />
 				<Contact />
 			</main>
 			<LayoutFooter />
